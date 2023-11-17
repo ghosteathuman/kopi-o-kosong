7a49f78fffd6dc0170aaffab034c2e3cf504c9f0
ghosteathuman <46582963+ghosteathuman@users.noreply.github.com>
2023-11-17 16:50:17 +0100

    Migrate Boards controller to use url_id when possible.

--- test/models/board_test.rb
+++ test/models/board_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :bigint           not null, primary key
+#  public     :boolean          default(FALSE)
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  url_id     :string           not null
+#
+# Indexes
+#
+#  index_boards_on_url_id  (url_id)
+#
 require "test_helper"
 
 class BoardTest < ActiveSupport::TestCase
